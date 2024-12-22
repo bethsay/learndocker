41f09a969c10e3a81009cf6f43afaa94fc2d59d1
bethsay <bethsay.tv@gmail.com>
2024-12-22 20:03:45 +0530

    learn docker-compose.yml with visits_project

--- visits_project/index.js
+++ visits_project/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const redis = require('redis');
+//const process = require('process'); //enable this when testing resiliance 
 
 const app = express();
-const client = redis.createClient();
-client.set('visits', 0);
+const client = redis.createClient({
+  host: 'redis-server', //redis service name as per docker-compose.yml. In a non-docker setup, IP/hostname/url of redis server must be defined.
+  port: 6379            //default redis port. can be skipped
+});
+client.set('visits', 0); //starting value of counter
 
 app.get('/', (req, res) => {
+//  process.exit(0); //enable this to test resiliance. 0 means exit without failure. Any other code means there is error/failure.
   client.get('visits', (err, visits) => {
     res.send('Number of visits is ' + visits);
-    client.set('visits', parseInt(visits) + 1);
+    client.set('visits', parseInt(visits) + 1); // increment count everytime app called in browser
   });
 });
 
